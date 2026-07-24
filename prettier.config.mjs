d68544f5fd882065208cf6156fb19865973e5f8b
Lam <daibanglam@gmail.com>
2026-07-24 09:10:37 +0700

    feat: set up Prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 85,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
+
+export default config;
