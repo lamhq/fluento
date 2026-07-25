0d56ca6791abcc779f34eff925f92e989c4e0a75
Lam <daibanglam@gmail.com>
2026-07-25 16:05:13 +0700

    feat: add api gateway app

---
+++ apps/api-gateway/healthcheck.mjs
@@ -0,0 +1,38 @@
+import http from 'http';
+
+async function checkStatus() {
+  return new Promise((resolve, reject) => {
+    const req = http.request(
+      {
+        hostname: 'localhost',
+        port: process.env.PORT || 5602,
+        path: '/api-gtw-health',
+        method: 'GET',
+      },
+      (res) => {
+        if (res.statusCode !== 200) {
+          reject(new Error(`Unexpected status code: ${res.statusCode}`));
+        } else {
+          resolve(`Status OK: ${res.statusCode}`);
+        }
+      },
+    );
+
+    req.on('error', (err) => {
+      reject(err);
+    });
+
+    req.end();
+  });
+}
+
+(async () => {
+  try {
+    const result = await checkStatus();
+    console.log(result);
+    process.exit(0);
+  } catch (err) {
+    console.error('Request failed:', err.message);
+    process.exit(1);
+  }
+})();
