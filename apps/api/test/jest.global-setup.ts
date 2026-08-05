7c89368d8cc959a0659b8ec179e8b9dd798686c6
Lam <daibanglam@gmail.com>
2026-08-05 19:42:34 +0700

    fix: api e2e test on CI

--- apps/api/test/jest.global-setup.ts
+++ apps/api/test/jest.global-setup.ts
@@ -4,11 +4,11 @@ import 'dotenv/config';
 import path from 'path';
 import { GenericContainer, Wait } from 'testcontainers';
 
-export default async function globalSetup() {
-  await startMockServer();
-}
+import deferred from './utils/deferred';
 
-async function startMockServer() {
+export default async function startMongoDB() {
+  console.log(`Start MongoDB`);
+
   const container = await new GenericContainer('mongo:4.2')
     .withEnvironment({
       MONGO_INITDB_ROOT_USERNAME: 'admin',
@@ -25,33 +25,37 @@ async function startMockServer() {
         target: '/docker-entrypoint-initdb.d/init-rs.js',
       },
     ])
-    .withWaitStrategy(Wait.forLogMessage('waiting for connection'))
-    // .withAutoCleanup(false)
+    .withWaitStrategy(Wait.forLogMessage('waiting for connections'))
     .withCommand(['--replSet', 'rs0', '--bind_ip_all'])
-    // .withLogConsumer((stream) => {
-    //   stream.on('data', (line) => {
-    //     console.log(line);
-    //   });
-    //   stream.on('err', (line) => {
-    //     console.error(line);
-    //   });
-    //   stream.on('end', () => {
-    //     console.log('Stream closed');
-    //   });
-    // })
     .start();
 
-  // initialize replica set for MongoDB
-  await new Promise((resolve) => setTimeout(resolve, 3000)); // wait for MongoDB to start
-  await container.exec([
-    'mongo',
-    'mongodb://admin:123123@localhost:27017/test?authSource=admin&directConnection=true',
-    '/docker-entrypoint-initdb.d/init-rs.js',
-  ]);
-  // const { stdout, stderr, exitCode } = await container.exec([
-  // console.log(`Command stdout: ${stdout}`);
-  // console.log(`Command stderr: ${stderr}`);
-  // console.log(`Command exit code: ${exitCode.toString()}`);
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // wait for replica set to initialize
-  // // const { output, stdout, stderr, exitCode } = await dbContainer.exec([
+  // Initialize replica set and wait until it is ready
+  const {
+    promise: rsPromise,
+    resolve: resolveRs,
+    reject: rejectRs,
+  } = deferred<undefined>();
+  const logStream = await container.logs();
+  logStream
+    .on('data', (line) => {
+      if (typeof line !== 'string') return;
+
+      if (line.includes('waiting for connections')) {
+        // Initialize replica set for MongoDB
+        console.log(`Initialize replica set`);
+        void container.exec([
+          'mongo',
+          'mongodb://admin:123123@localhost:27017/test?authSource=admin&directConnection=true',
+          '/docker-entrypoint-initdb.d/init-rs.js',
+        ]);
+      } else if (line.includes('database writes are now permitted')) {
+        console.log(`Replica set is ready`);
+        resolveRs(undefined);
+      }
+    })
+    .on('error', (line) => {
+      console.error(line);
+      rejectRs(new Error('Replica set initialization failed'));
+    });
+  return rsPromise;
 }
