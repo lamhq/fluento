0fec1ce6b4f7e30f898a8562214c0c449ecfaa9b
Lam <daibanglam@gmail.com>
2026-08-21 18:22:05 +0700

    feat: integrate shadcn

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types="vitest/config" />
+import path from 'node:path';
+
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
@@ -7,6 +9,11 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src/shadcn'),
+    },
+  },
   server: {
     open: false,
     proxy: {
